b58195149710b326e83411505a3bc077ac76d406
Dmitry <gapeevdirog@gmail.com>
2021-05-23 21:32:28 +0300

    added CatalogContext

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+
+        public CatalogContext(string connectionString, string databaseName, string collectionName)
+        {
+            var client = new MongoClient(connectionString);
+            var db = client.GetDatabase(databaseName);
+
+            Products = db.GetCollection<Product>(collectionName);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
