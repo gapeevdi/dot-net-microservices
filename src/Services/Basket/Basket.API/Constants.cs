109f23fd77c20da1ffd3ea58e555e102c32e7653
DmitryH <HAPEYEU.DZMITRY@P97.COM>
2021-06-04 13:18:18 +0300

    added Constants

---
+++ src/Services/Basket/Basket.API/Constants.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Basket.API
+{
+    public static class Constants
+    {
+        public static class Redis
+        {
+
+            public static class Environment
+            {
+                public const string Host = "REDIS_HOST";
+                public const string Port = "REDIS_PORT";
+            }
+
+            public const string DefaultHost = "localhost";
+            public const int DefaultPort = 27017;
+        }
+    }
+}
--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -19,7 +19,10 @@ namespace Basket.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddStackExchangeRedisCache((options) =>
+            {
+                options.Configuration = 
+            });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
