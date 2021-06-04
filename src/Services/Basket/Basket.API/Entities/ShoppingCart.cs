a87fc01e7667eeab396b6c958e4fa63c94849686
DmitryH <HAPEYEU.DZMITRY@P97.COM>
2021-06-04 12:57:03 +0300

    added Basket entities

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Basket.API.Entities
+{
+
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public decimal TotalPrice => CalculateTotal();
+
+        private decimal CalculateTotal() =>
+            Items.Sum(item => item.Price * item.Quantity);
+    }
+}
