a91efe1d632131f6c394949865fcd76feeb9840b
DmitryH <HAPEYEU.DZMITRY@P97.COM>
2021-06-09 18:04:11 +0300

    added DiscountRepository

---
+++ src/Services/Discount/Discount.API/Repositories/IDiscountRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Discount.API.Entities;
+
+namespace Discount.API.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> Get(string productName);
+        Task Create(Coupon coupon);
+        Task Update(Coupon coupon);
+        Task Delete(string productName);
+    }
+}
