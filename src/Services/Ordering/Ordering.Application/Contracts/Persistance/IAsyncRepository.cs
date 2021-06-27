1e539482ea139929eac948516028dfefc7e4a396
DmitryH <HAPEYEU.DZMITRY@P97.COM>
2021-06-27 21:28:20 +0300

    added abstraction for application services

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistance/IAsyncRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Ordering.Domain.Common;
+
+namespace Ordering.Application.Contracts.Persistance
+{
+    public interface IAsyncRepository<T>
+        where T : EntityBase
+    {
+        Task<IReadOnlyList<T>> GetAll();
+        Task<IReadOnlyList<T>> Get(Expression<Func<T, bool>> predicate);
+
+        Task<IReadOnlyList<T>> Get(Expression<Func<T, bool>> predicate = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string include = null, bool trackEntity = true);
+
+        Task<IReadOnlyList<T>> Get(Expression<Func<T, bool>> predicate = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            IReadOnlyList<Expression<Func<T, object>>> includes = null, 
+            bool trackEntity = true);
+
+        Task<T> GetById(int id);
+
+        Task<T> Add(T entity);
+        Task Update(T entity);
+        Task Delete(T entity);
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistance/IOrderedRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Contracts.Persistance
+{
+    public interface IOrderedRepository : IAsyncRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
+
+    }
+}
