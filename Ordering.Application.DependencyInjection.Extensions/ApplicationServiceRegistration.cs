3fbc7e9a903259db287ecc933ebc949c40a6cc0b
DmitryH <HAPEYEU.DZMITRY@P97.COM>
2021-06-28 15:27:14 +0300

    created Ordering.Application.DependencyInjection.Extensions

---
+++ Ordering.Application.DependencyInjection.Extensions/ApplicationServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviors;
+using Ordering.Application.Mappings;
+
+namespace Ordering.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            var applicationAssembly = typeof(MappingProfile).Assembly;
+
+            services.AddAutoMapper(applicationAssembly);
+            services.AddValidatorsFromAssembly(applicationAssembly);
+            services.AddMediatR(applicationAssembly);
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+            return services;
+        }
+    }
+}
