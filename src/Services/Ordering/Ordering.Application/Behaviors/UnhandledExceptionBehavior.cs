92acc01c5ad2f7d054b11d0a76b72525f2731de3
DmitryH <HAPEYEU.DZMITRY@P97.COM>
2021-06-28 13:30:42 +0300

    added validation for commands, UnhandledException handler

---
+++ src/Services/Ordering/Ordering.Application/Behaviors/UnhandledExceptionBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Ordering.Application.Behaviors
+{
+    public class UnhandledExceptionBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly ILogger<TRequest> _logger;
+
+        public UnhandledExceptionBehavior(ILogger<TRequest> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Application exception, Unhandled exception for Request {typeof(TRequest).Name},  {request}");
+                throw;
+            }
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Behaviors/ValidationBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
+using MediatR;
+
+namespace Ordering.Application.Behaviors
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+
+        private readonly IReadOnlyCollection<IValidator<TRequest>> _validators;
+
+
+        public ValidationBehavior(IReadOnlyCollection<IValidator<TRequest>> validators)
+        {
+            _validators = validators ?? throw new ArgumentNullException(nameof(validators));
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            await RunValidation(request, cancellationToken);
+            return await next();
+        }
+
+        private async Task RunValidation(TRequest request, CancellationToken cancellationToken)
+        {
+            if(_validators.Any() == false) return;
+
+            var context = new ValidationContext<TRequest>(request);
+
+            var validationResults = await Task.WhenAll(_validators.Select(validator => validator.ValidateAsync(context, cancellationToken)));
+            var failures = validationResults.SelectMany(result => result.Errors).ToList();
+
+            if (failures.Any())
+            {
+                throw new ValidationException(failures);
+            }
+
+        }
+    }
+}
