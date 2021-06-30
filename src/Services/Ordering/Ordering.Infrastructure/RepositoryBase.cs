3c6889c5bcc4ce3eeca76ccb3a20ac07ae0f8cdd
DmitryH <HAPEYEU.DZMITRY@P97.COM>
2021-06-30 14:10:14 +0300

    added RepositoryBase

---
+++ src/Services/Ordering/Ordering.Infrastructure/RepositoryBase.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Persistance;
+using Ordering.Domain.Common;
+using Ordering.Infrastructure.Persistence;
+
+namespace Ordering.Infrastructure
+{
+    public class RepositoryBase<T> : IAsyncRepository<T>
+        where T : EntityBase
+    {
+        private readonly OrderContext _context;
+
+        public RepositoryBase(OrderContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<IReadOnlyList<T>> GetAll()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> Get(Expression<Func<T, bool>> predicate)
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> Get(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string include = null, bool trackEntity = true)
+        {
+            IQueryable<T> query = _context.Set<T>();
+            if (trackEntity == false)
+            {
+                query = query.AsNoTracking();
+            }
+
+            if (string.IsNullOrEmpty(include) == false)
+            {
+                query = query.Include(include);
+            }
+
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+
+            if (orderBy != null)
+            {
+                return await orderBy(query).ToListAsync();
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> Get(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, IReadOnlyList<Expression<Func<T, object>>> includes = null, bool trackEntity = true)
+        {
+            IQueryable<T> query = _context.Set<T>();
+            if (trackEntity == false)
+            {
+                query = query.AsNoTracking();
+            }
+
+            if (includes != null)
+            {
+                query = includes.Aggregate(query, (current, include) => current.Include(include));
+            }
+
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+
+            if (orderBy != null)
+            {
+                return await orderBy(query).ToListAsync();
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<T> Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
