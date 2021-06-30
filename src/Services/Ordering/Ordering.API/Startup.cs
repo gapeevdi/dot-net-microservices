d4be54993dc36eb5cf338b5cc1c08ee76021443f
DmitryH <HAPEYEU.DZMITRY@P97.COM>
2021-06-30 17:21:46 +0300

    added connection string configuration

--- src/Services/Ordering/Ordering.API/Startup.cs
+++ src/Services/Ordering/Ordering.API/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Ordering.Application;
+using Ordering.Infrastructure;
 
 namespace Ordering.API
 {
@@ -28,6 +29,7 @@ namespace Ordering.API
             });
 
             services.AddApplicationServices();
+            services.AddInfrastructureServices(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
