e1f99798fae09d2dbd0e02b9a809e62d6a005534
DmitryH <HAPEYEU.DZMITRY@P97.COM>
2021-07-04 21:12:30 +0300

    added event classes

---
+++ src/Common/EventBus.Messages/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace EventBus.Messages
+{
+    public static class Constants
+    {
+        public static class Queues
+        {
+            public const string BasketCheckoutQueue = "basketcheckout-queue";
+        }
+    }
+}
---
+++ src/Common/EventBus.Messages/Events/IntegrationBaseEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EventBus.Messages.Events
+{
+    public abstract class IntegrationBaseEvent
+    {
+        protected IntegrationBaseEvent()
+        {
+            Id = Guid.NewGuid();
+            CreationDate = DateTime.UtcNow;
+        }
+
+        protected IntegrationBaseEvent(Guid id, DateTime creationDate)
+        {
+            Id = id;
+            CreationDate = creationDate;
+        }
+
+        public Guid Id { get; }
+        public DateTime CreationDate { get; }
+    }
+}
