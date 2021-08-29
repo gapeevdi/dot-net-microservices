7f9aae136703b0edd9c6eecb5ccf4bb50c278261
Dmitry Gapeev <gapeevdirog@gmail.com>
2021-08-29 23:40:07 +0300

    removed extra lines from Ocelot.StartUp

--- src/ApiGateways/ApiGateway/Startup.cs
+++ src/ApiGateways/ApiGateway/Startup.cs
@@ -20,21 +20,6 @@ namespace ApiGateway
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
-
             await app.UseOcelot();
         }
     }
